7d0f63d77e2a3bdf28168ceb9ad3e00b4cde6d53
subin1o24 <suebin1024@gmail.com>
2026-05-30 23:51:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-# 게시글 저장 리스트
 posts = []
 
 
-# 메인 페이지
 @app.route('/')
 def index():
 
@@ -24,26 +22,20 @@ def index():
     <h3>게시글 목록</h3>
     """
 
-    # 메인 페이지는 안전하게 출력
     for post in posts:
         html += f"<p>{escape(post)}</p>"
 
     return html
 
 
-# 게시글 저장
 @app.route('/write', methods=['POST'])
 def write():
 
     content = request.form['content']
-
-    # 입력값 저장
     posts.append(content)
-
     return redirect('/')
 
 
-# 관리자 페이지
 @app.route('/admin')
 def admin():
 
@@ -55,7 +47,6 @@ def admin():
     <h3>저장된 게시글 확인</h3>
     """
 
-    # 관리자 페이지는 취약하게 출력
     for post in posts:
         html += f"<p>{post}</p>"
 
